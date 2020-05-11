667f50881392f7969ff85fe8b22c1a2293883133
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-05-11 22:37:54 +0200

    remove item on press

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" }
+  ]);
+
+  const pressHandler = (key: String) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.key != key));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <TodoItem {...{ item, pressHandler }} />}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +32,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  content: {
+    padding: 40
+  },
+  list: { marginTop: 20 }
 });
